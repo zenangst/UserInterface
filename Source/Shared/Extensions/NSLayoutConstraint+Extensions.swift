545674de29aab9c3d507118e400afb0a4f3e43af
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-04-30 08:36:46 +0200

    Improve layout constraints extensions

--- Source/Shared/Extensions/NSLayoutConstraint+Extensions.swift
+++ Source/Shared/Extensions/NSLayoutConstraint+Extensions.swift
@@ -5,22 +5,29 @@
 #endif
 
 public extension NSLayoutConstraint {
-  public static func constrain(_ constraints: NSLayoutConstraint?...) {
+  @discardableResult public static func constrain(activate: Bool = true, _ constraints: NSLayoutConstraint?...) -> [NSLayoutConstraint?] {
     for constraint in constraints {
       (constraint?.firstItem as? View)?.translatesAutoresizingMaskIntoConstraints = false
     }
 
-    NSLayoutConstraint.activate(constraints.compactMap { $0 })
+    if activate {
+      NSLayoutConstraint.activate(constraints.compactMap { $0 })
+    }
+
+    return constraints
   }
 
   public static func pin(_ view: View,
                   toView: View,
-                  insets: EdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 0)) {
-    NSLayoutConstraint.constrain(
+                  activate: Bool = true,
+                  insets: EdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 0)) -> [NSLayoutConstraint?] {
+    let constraints = NSLayoutConstraint.constrain(activate: activate,
       view.leadingAnchor.constraint(equalTo: toView.leadingAnchor, constant: insets.left),
       view.trailingAnchor.constraint(equalTo: toView.trailingAnchor, constant: -insets.right),
       view.topAnchor.constraint(equalTo: toView.topAnchor, constant: insets.top),
       view.bottomAnchor.constraint(equalTo: toView.bottomAnchor, constant: -insets.bottom)
     )
+
+    return constraints
   }
 }
--- Source/iOS+tvOS/Extensions/NSLayoutConstraints+iOS.swift
+++ Source/iOS+tvOS/Extensions/NSLayoutConstraints+iOS.swift
@@ -1,12 +1,17 @@
 import UIKit
 
 public extension NSLayoutConstraint {
-  public static func pin(_ layoutGuide: UILayoutGuide, toView: View, insets: EdgeInsets = .zero) {
-    NSLayoutConstraint.constrain(
+  @discardableResult public static func pin(_ layoutGuide: UILayoutGuide,
+                         toView: View,
+                         activate: Bool = true,
+                         insets: EdgeInsets = .zero) -> [NSLayoutConstraint?] {
+    let constraints = NSLayoutConstraint.constrain(activate: activate,
       layoutGuide.leadingAnchor.constraint(equalTo: toView.leadingAnchor, constant: insets.left),
       layoutGuide.trailingAnchor.constraint(equalTo: toView.trailingAnchor, constant: -insets.right),
       layoutGuide.topAnchor.constraint(equalTo: toView.topAnchor, constant: insets.top),
       layoutGuide.bottomAnchor.constraint(equalTo: toView.bottomAnchor, constant: -insets.bottom)
     )
+
+    return constraints
   }
 }
