531d50f2d17d4424b3fd0254aacea5523802ed11
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-05-16 07:42:10 +0200

    Add addAndPin method on NSLayoutConstraint

--- Source/iOS+tvOS/Extensions/NSLayoutConstraints+iOS.swift
+++ Source/iOS+tvOS/Extensions/NSLayoutConstraints+iOS.swift
@@ -1,6 +1,22 @@
 import UIKit
 
 public extension NSLayoutConstraint {
+  /// Add layoutGuide as a subview an pin constraints to parent view.
+  ///
+  /// - Parameters:
+  ///   - layoutGuide: The layout guide that should be added and pinned.
+  ///   - toView: The view that the should act as the parent for constraints.
+  ///   - activate: Indicates if the constraints should be activated or not.
+  ///   - insets: Insets that are applied as constants for the constraints.
+  /// - Returns: A collection of layout constraints.
+  @discardableResult public static func addAndPin(_ layoutGuide: UILayoutGuide,
+                                                  toView: View,
+                                                  activate: Bool = true,
+                                                  insets: EdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 0))  -> [NSLayoutConstraint?] {
+    toView.addLayoutGuide(layoutGuide)
+    return pin(layoutGuide, toView: toView, activate: activate, insets: insets)
+  }
+
   /// Pin a layout guide to a view using constraints.
   ///
   /// - Parameters:
