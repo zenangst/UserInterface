759d17313bf5302ad195d85785d46267cf8a0abf
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-09-27 09:13:27 +0200

    Move addSubview(_,pin:insets:) to shared

--- Source/iOS+tvOS/Extensions/UIView+Extensions.swift
+++ Source/iOS+tvOS/Extensions/UIView+Extensions.swift
@@ -6,11 +6,6 @@ public extension UIView {
     self.backgroundColor = backgroundColor
   }
 
-  @discardableResult
-  public func addSubview(_ view: UIView, pin: Bool = false, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
-    return NSLayoutConstraint.addAndPin(view, toView: self, insets: insets)
-  }
-
   public func add(_ shadow: Shadow) {
     layer.shadowColor = shadow.color.cgColor
     layer.shadowRadius = shadow.radius
